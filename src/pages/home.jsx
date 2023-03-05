ed07c5b061625a8a6e9fa9ade79aa90bdf8277ec
Decagon <uchecharles223@gmail.com>
2023-03-06 00:11:23 +0100

    worked on the widget and chart component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./home.css";
 import FeaturedInfo from "../components/featuredInfo/FeaturedInfo";
+import Chart from "../components/chart/chart";
+import { userData } from "../dummyData.js";
+import WidgetSm from "../components/widgetSm/WidgetSm";
+import WidgetLg from "../components/widgetLg/WidgetLg";
 
 export default function home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
